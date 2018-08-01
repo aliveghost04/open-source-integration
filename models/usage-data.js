0e40bed10d457efead549cb60eb8fd3336f07f48
Erick Jiménez <erick.jimenezr04@gmail.com>
2018-07-31 23:25:37 -0400

    fix bug related with usage-data module

--- models/usage-data.js
+++ models/usage-data.js
@@ -16,7 +16,7 @@ module.exports = function UsageDataModel(Model) {
           ]
         );
     },
-    get(user = '', from, to, limit = 250) {
+    get(user, from, to, limit = 250) {
       return Model
         .getConnection()
         .then((connection) => {
@@ -24,28 +24,47 @@ module.exports = function UsageDataModel(Model) {
           let fromDate = from || mysql.raw('DATE_SUB(CURDATE(), INTERVAL 3 MONTH)');
           let toDate = to || mysql.raw('CURDATE()');
 
-          return Model
-            .query(
-              `SELECT u.user, ud.ip, ud.requested_service, ud.access_date
-              FROM usage_data ud
-              INNER JOIN users u
-                ON u.id = ud.user
-              WHERE (
-                  ud.user = ?
-                  OR u.fullname = ?
-                )
-                AND DATE(access_date) >= ?
-                AND DATE(access_date) <= ?
-              ORDER BY access_date DESC
-              LIMIT ?`,
-              [
-                user,
-                user,
-                fromDate,
-                toDate,
-                limit
-              ]
-            );
+          if (user) {
+            return Model
+              .query(
+                `SELECT u.user, ud.requested_service, ud.access_date
+                FROM usage_data ud
+                INNER JOIN users u
+                  ON u.id = ud.user
+                WHERE (
+                    ud.user = ?
+                    OR u.fullname = ?
+                  )
+                  AND DATE(access_date) >= ?
+                  AND DATE(access_date) <= ?
+                ORDER BY access_date DESC
+                LIMIT ?`,
+                [
+                  user,
+                  user,
+                  fromDate,
+                  toDate,
+                  limit
+                ]
+              );
+          } else {
+            return Model
+              .query(
+                `SELECT u.user, ud.ip, ud.requested_service, ud.access_date
+                FROM usage_data ud
+                INNER JOIN users u
+                  ON u.id = ud.user
+                WHERE DATE(access_date) >= ?
+                  AND DATE(access_date) <= ?
+                ORDER BY access_date DESC
+                LIMIT ?`,
+                [
+                  fromDate,
+                  toDate,
+                  limit
+                ]
+              );
+          }
         });
     }
   };
